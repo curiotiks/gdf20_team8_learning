67314e59ce440bfe4783ab5ee9158b0b02cdd5ca
almhere <anita.lee.mitchell@gmail.com>
2020-11-08 13:43:57 -0500

    Moving NPC
    moving 'NPC sick', still needs physics tweaked

---
+++ Assets/OurProject/Scripts/SickMovingScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SickMovingScript : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D myRigidBody;
+    private bool moving;
+    public float timeBetweenMove;
+    private float timeBetweenMoveCounter;
+    public float timeToMove;
+    private float timeToMoveCounter;
+    private Vector3 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        timeBetweenMoveCounter = timeBetweenMove;
+        timeToMoveCounter = timeBetweenMove;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+        {
+            timeToMoveCounter -= Time.deltaTime;
+            myRigidBody.velocity = moveDirection;
+            if (timeToMoveCounter < 0f)
+            {
+                moving = false;
+                timeBetweenMoveCounter = timeBetweenMove;
+            }
+
+        }
+        else
+        {
+            timeBetweenMoveCounter -= Time.deltaTime;
+            myRigidBody.velocity = Vector2.zero;
+            if (timeBetweenMoveCounter < 0f)
+            {
+                moving = true;
+                timeToMoveCounter = timeToMove;
+                moveDirection = new Vector3(Random.Range(-1f, 1f) * moveSpeed, Random.Range(-1f, 1f) * moveSpeed, 0f);
+            }
+        }
+
+    }
+}
