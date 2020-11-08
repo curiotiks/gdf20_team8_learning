7c95375a59a9862e3984a184990f621610a4612b
almhere <anita.lee.mitchell@gmail.com>
2020-11-08 11:52:28 -0500

    Scene switch
    Add scene switch, had to turn off fading sprite system so roofs aren't working

---
+++ Assets/OurProject/Scripts/ToCovidiaScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToCovidiaScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // trigger next scene
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
