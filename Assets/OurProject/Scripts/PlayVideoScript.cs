2ec37bb1514c328fc87fc7a8c7c69541a9ded86b
almhere <anita.lee.mitchell@gmail.com>
2020-12-06 20:23:50 -0500

    link to play video on main menu

---
+++ Assets/OurProject/Scripts/PlayVideoScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideoScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PlayVideoURL()
+    {
+        Application.OpenURL("https://youtu.be/EUNvmQSiTAs");
+    }
+}
---
+++ Assets/OurProject/Scripts/ToVideoScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToVideoScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PlayTheVideo()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
